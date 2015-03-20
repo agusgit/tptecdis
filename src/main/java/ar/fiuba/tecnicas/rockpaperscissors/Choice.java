325458f799113a44e395d3f5dea756f6896a9ec8
FreakAthox mortax <freakathoxmortax@freakathox-mortaxs-macbook-pro.local>
2015-03-20 16:49:23 -0700

    Agrego header en la Interface

--- src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Choice.java
+++ src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Choice.java
@@ -1,5 +1,13 @@
 package ar.fiuba.tecnicas.rockpaperscissors;
 
+/**
+ * Esta interface debe ser implementada por todas la elecciones posibles del juego
+ * Rock-Paper-Scissors
+ * 
+ * @author Agustin
+ *
+ */
+
 public interface Choice {
 	
 	public Choice vs(Choice someChoice);
